e08a7fde41ad2ab250efa08d29210801aeda7b9d
Miran-Mirantee <killerm11805@hotmail.com>
2025-07-14 17:20:42 +0700

    setup project

---
+++ src/components/Todo.tsx
@@ -0,0 +1,12 @@
+function Todo() {
+  // This is a placeholder component for the Todo functionality
+  // It currently does not have any logic or UI elements
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <p>This is where the todo list will be implemented.</p>
+    </div>
+  );
+}
+
+export default Todo;
